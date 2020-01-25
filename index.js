310ba16c095832d15059d3f7f299996150383930
Matthew Wester <MatthewWester@live.com>
2020-01-25 13:15:48 -0700

    Now function does basic call for github information

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const util = require("util");
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
+function developerProfile(username, color) {
+    this.username = username;
+    this.color= color;
+}
+
 inquirer
     .prompt([
         {
@@ -18,11 +23,27 @@ inquirer
             message: "What color scheme would you like for your generated profile?",
             choices: [ "Red", "Blue", "Green"],
             name: "colorChoice"
-
-        }
+        },
     ])
     .then(function(response) {
-        console.log(response.username);
-        console.log(response.colorChoice);
+        axios
+            .get("https://api.github.com/users/" + response.username)
+            .then(function(res) {
+                console.log(res.data.login);
+                console.log(res.data.avatar_url);
+                console.log(res.data.bio);
+                console.log(res.data.location);
+                console.log(res.data.public_repos);
+                console.log(res.data.followers);
+                console.log(res.data.following);
+                console.log(res.public_gists);
+                console.log(res.data.public_repos);
+                console.log(res.data.blog);
+
+
+                // const newProfile = new developerProfile(response.username, response.colorChoice);
+                // console.log(newProfile);
+            });
         });
 
+ 
